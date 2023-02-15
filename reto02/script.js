fa86e120e09bab7a354d824e5dfd723199c98264
jpzingoni <juanzingoni@gmail.com>
2023-02-15 12:11:00 -0300

    Se agrega algunas funciones con JavaScript para poder realizar algunas interacciones

--- reto02/script.js
+++ reto02/script.js
@@ -4,16 +4,32 @@ let $conteinerBuscador = document.getElementById("conteiner-buscador");
 let $botonesBuscador = document.getElementById("botones-buscador");
 let $main = document.getElementById("conteiner-total");
 let $busquedas = document.getElementById("busquedas");
-
+let $appButton = document.getElementById("showAppButton");
+let $chocolateMenu = document.getElementById("chocolate-menu");
+let contadorMenuChocolate = 0;
+let contadorInput = 0;
 $input.addEventListener("click", function(){
     $conteinerBuscador.classList.add("agregadojs");
     $buscador.classList.add("agregadojs");
     $busquedas.style.display = "block";
+    console.log(contadorInput)
 })
 
-// $main.addEventListener("click", function(){
-//     console.log("hola");
-//     if($busquedas.style.display == "block"){
-//         return $busquedas.style.display = "none";
-//     }
-// })
+$appButton.addEventListener("click", function(){
+    $chocolateMenu.style.display = "flex";
+    console.log(contadorMenuChocolate);
+})
+
+$main-addEventListener("click", function(){
+    console.log(contadorMenuChocolate);
+    contadorMenuChocolate++;
+    contadorInput++;
+    if(contadorMenuChocolate > 1){
+        $chocolateMenu.style.display = "none";
+        contadorMenuChocolate = 0;
+    }
+    if(contadorInput > 1){
+        $busquedas.style.display = "none";
+        contadorInput = 0;
+    }
+})
